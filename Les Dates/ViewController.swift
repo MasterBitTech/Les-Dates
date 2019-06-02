51147478cb12c5ff279caced9c63c71e6688fb1c
MasterBitTech <cauchy.benoit@icloud.com>
2019-06-02 14:53:23 -0400

    début défi

--- Les Dates/ViewController.swift
+++ Les Dates/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    //Obtenir une string au faormat de votre choix
+    @IBOutlet weak var dateStringLbl: UILabel!
+    
+    //Time Interval (un peu de recherche)
+    @IBOutlet weak var timeIntervalLbl: UILabel!
+    
+    //Si aujourd'hui, demain, hier, week-end
+    @IBOutlet weak var calendarLbl: UILabel!
+    
+    //Il y a + mois, jours, heures, minutes etc
+    @IBOutlet weak var ilyaLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
 
+    @IBAction func dateChoisie(_ sender: UIDatePicker) {
+        let date = sender.date
+        dateStringLbl.text = date.convertirDate(date: date)
+        // print(date)
+    }
 }
 
+extension Date {
+
+    func convertirDate(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = DateFormatter.Style.full
+        dateFormatter.timeStyle = .medium
+        dateFormatter.locale = Locale(identifier: "fr_CA")
+        
+        return dateFormatter.string(from: date)
+    }
+
+}
